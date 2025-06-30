0c6a72178b6b5ac7c07528f43ac3ef842aadd56d
Thilina Ruwan <payshiasoftwaresolutions@gmail.com>
2025-06-30 16:25:11 +0000

    add counters adn Why Choose Us  sections to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,16 @@ import Hero from '@/components/home/Hero';
 import Features from '@/components/home/Features';
 import FeaturedInstitutes from '@/components/home/FeaturedInstitutes';
 import Cta from '@/components/home/Cta';
+import Counters from '@/components/home/Counters';
+import WhyChooseUs from '@/components/home/WhyChooseUs';
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <Hero />
       <Features />
+      <Counters />
+      <WhyChooseUs />
       <FeaturedInstitutes />
       <Cta />
     </div>
